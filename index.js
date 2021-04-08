948a2df3f82e925cba1ba136511a0ee90d9b9382
박용환 <longhuan@hatiolab.com>
2021-04-08 14:44:59 +0900

    fixlogic

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
+const { setInterval,setTimeout } = require( "timers");
+
 const { S7Endpoint, S7ItemGroup } = require("@st-one-io/nodes7");
 
-let plc = new S7Endpoint({ host: "192.168.0.1", rack: 0, slot: 1 });
+let plc = new S7Endpoint({ host: "10.0.116.52"});
 
 plc.on("error", (e) => console.log("PLC Error!", e));
 plc.on("disconnect", () => console.log("PLC Disconnect"));
@@ -14,18 +16,23 @@ plc.once("connect", async () => {
   // you can use the S7ItemGroup to perform optimized read/write of variables
 
   let itemGroup = new S7ItemGroup(plc);
+  for (let index = 0; index < 10; index++) {
+    await delay(3000)
 
-  let vars = {
-    TEST1: "DB260,W16", // word real at DB260,W16
-    TEST3: "DB260,X6.1", // Bit at DB260,X6.1
-    TEST3: "DB260,X6.7", // Bit at DB260,X6.7
-    TEST2: "DB260,X7.0", // Bit at DB260,X7.0
-    TEST3: "DB260,X7.1", // Bit at DB260,X7.1
-  };
-  itemGroup.setTranslationCB((tag) => vars[tag]); //translates a tag name to its address
-  itemGroup.addItems(Object.keys(vars));
+    let vars = {
+      W16: "DB260,W16", // word real at DB260,W16
+      "X6.1": "DB260,X6.1", // Bit at DB260,X6.1
+      "X6.7": "DB260,X6.7", // Bit at DB260,X6.7
+      "X7.0": "DB260,X7.0", // Bit at DB260,X7.0
+      "X7.1": "DB260,X7.1", // Bit at DB260,X7.1
+    };
+    itemGroup.setTranslationCB((tag) => vars[tag]); //translates a tag name to its address
+    itemGroup.addItems(Object.keys(vars));
+  
+    console.log(await itemGroup.readAllItems());
+  }
+  await plc.disconnect();
 
-  console.log(await itemGroup.readAllItems());
-
-  await plc.disconnect(); //clean disconnection
 });
+
+const delay = ms => new Promise(res=>setTimeout(res,ms))
